048c66de4589b8c4930367e0ac275d1680562c7a
GauBen <benaim.gautier@gmail.com>
2021-03-06 10:23:42 +0100

    feat: glitch effect

--- assets/splash.ts
+++ assets/splash.ts
@@ -3,9 +3,11 @@ const $ = document.querySelector.bind(document)
 const $title = $('.splash-title')
 const $words = $('.splash-title > .words')
 
-$title.style.setProperty('--scale', window.innerHeight / $title.offsetHeight)
-$title.style.setProperty('--translateY', window.innerHeight / 2 - $title.offsetTop - $title.offsetHeight / 2)
-// $title.style.setProperty('--width', $title.offsetWidth)
-$title.style.setProperty('--translateX', $words.offsetWidth)
+const computeOffset = (e, prop) => e[prop] + (e.offsetParent ? computeOffset(e.offsetParent, prop) : 0)
 
+document.body.style.setProperty('--scale', window.innerHeight / $title.offsetHeight)
+document.body.style.setProperty('--translateY', window.innerHeight / 2 - computeOffset($title, 'offsetTop') - $title.offsetHeight / 2)
+document.body.style.setProperty('--translateX', $words.offsetWidth)
+
+document.body.classList.remove('-no-js')
 document.body.classList.add('-do-splash')
