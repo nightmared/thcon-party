bf00947fbb310a0bfe6af2c901abb5376e8c7e5c
GauBen <benaim.gautier@gmail.com>
2021-03-16 20:41:48 +0100

    feat: countdown and navbar

--- assets/splash.ts
+++ assets/splash.ts
@@ -2,6 +2,7 @@ const $ = document.querySelector.bind(document)
 
 const $title = $('.splash-title')
 const $words = $('.splash-title > .words')
+const $countdown = $('#countdown-value')
 
 const computeOffset = (e, prop) => e[prop] + (e.offsetParent ? computeOffset(e.offsetParent, prop) : 0)
 
@@ -9,5 +10,17 @@ document.body.style.setProperty('--scale', window.innerHeight / $title.offsetHei
 document.body.style.setProperty('--translateY', window.innerHeight / 2 - computeOffset($title, 'offsetTop') - $title.offsetHeight / 2)
 document.body.style.setProperty('--translateX', $words.offsetWidth)
 
-document.body.classList.remove('-no-js')
 document.body.classList.add('-do-splash')
+
+const update = () => {
+  const diff = new Date('2021-06-11T11:00:00.000Z') - new Date()
+  if (diff < 0) {
+    $countdown.innerHTML = '0!!'
+  } else {
+    const str = (diff / (1000 * 60 * 60 * 24)).toFixed(7)
+    $countdown.innerHTML = str
+    requestAnimationFrame(update)
+  }
+}
+
+update()
