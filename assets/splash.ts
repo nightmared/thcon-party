1e821725096fbddc5c7179cde9e152ec95a55924
GauBen <benaim.gautier@gmail.com>
2021-03-19 11:45:28 +0100

    feat: new font, logo and background

--- assets/splash.ts
+++ assets/splash.ts
@@ -6,14 +6,24 @@ const $countdown = $('#countdown-value')
 
 const computeOffset = (e, prop) => e[prop] + (e.offsetParent ? computeOffset(e.offsetParent, prop) : 0)
 
-document.body.style.setProperty('--scale', window.innerHeight / $title.offsetHeight)
-document.body.style.setProperty('--translateY', window.innerHeight / 2 - computeOffset($title, 'offsetTop') - $title.offsetHeight / 2)
-document.body.style.setProperty('--translateX', $words.offsetWidth)
+document.body.style.setProperty('--splash-scale', window.innerHeight / $title.offsetHeight)
+document.body.style.setProperty('--splash-translateY', window.innerHeight / 2 - computeOffset($title, 'offsetTop') - $title.offsetHeight / 2)
+document.body.style.setProperty('--splash-translateX', $words.offsetWidth)
 
-console.log(window.scrollY)
 if (window.scrollY <= 1 && window.location.hash.length === 0) {
   document.body.classList.add('-do-splash')
 }
+
+const scrollHandler = () => {
+  requestAnimationFrame(() =>
+    document.body.style.setProperty(
+      '--scroll',
+      (window.scrollY / window.innerHeight).toString()
+    ), 0
+  )
+}
+scrollHandler()
+window.addEventListener('scroll', e => scrollHandler(), { passive: true })
 
 const update = () => {
   const diff = new Date('2021-06-11T11:00:00.000Z') - new Date()
